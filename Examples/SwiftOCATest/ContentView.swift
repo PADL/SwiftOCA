75543994c836ecf3c0fecd7069390e91d738704f
Luke Howard <lukeh@padl.com>
2023-06-16 13:28:20 +1000

    move connection object up to App layer

--- Examples/SwiftOCATest/ContentView.swift
+++ Examples/SwiftOCATest/ContentView.swift
@@ -8,23 +8,19 @@
 import SwiftUI
 import SwiftOCA
 import SwiftOCAUI
-import Socket
 
 struct ContentView: View {
-    @StateObject var connection: AES70OCP1Connection
-
+    var connection: AES70OCP1Connection
     let id = OcaObjectIdentification(oNo: 0x9008, classIdentification: OcaMute.classIdentification)
 
-    init() {
-        let connection = AES70OCP1TCPConnection(deviceAddress: IPv4SocketAddress(address: IPv4Address(rawValue: "127.0.0.1")!, port: 65000))
-        self._connection = StateObject<AES70OCP1Connection>(wrappedValue: connection)
+    init(_ connection: AES70OCP1Connection) {
+        self.connection = connection
     }
 
     var body: some View {
         HStack {
             Text("Mute")
             Spacer()
-            
             OcaMuteView(connection, object: id)
         }
         .padding()
--- Examples/SwiftOCATest/SwiftOCATestApp.swift
+++ Examples/SwiftOCATest/SwiftOCATestApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftOCA
+import Socket
 
 extension OcaRoot: Identifiable {
     public var id: OcaONo { return self.objectNumber }
@@ -14,9 +15,16 @@ extension OcaRoot: Identifiable {
 
 @main
 struct SwiftOCATestApp: App {
+    @StateObject var connection: AES70OCP1Connection
+
+    init() {
+        let connection = AES70OCP1TCPConnection(deviceAddress: IPv4SocketAddress(address: IPv4Address(rawValue: "127.0.0.1")!, port: 65000))
+        self._connection = StateObject<AES70OCP1Connection>(wrappedValue: connection)
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(connection)
         }
     }
 }
