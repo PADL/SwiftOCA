9ea36689691598941b373f30505c8e8dc752069b
Luke Howard <lukeh@padl.com>
2023-06-30 14:12:30 +1000

    ensure objectWillChange runs on main thread, use DispatchQueue

--- Sources/SwiftOCA/OCC/PropertyTypes/Property.swift
+++ Sources/SwiftOCA/OCC/PropertyTypes/Property.swift
@@ -241,7 +241,9 @@ public struct OcaProperty<Value: Codable>: Codable, OcaPropertyChangeEventNotifi
             subject.send(.failure(error))
         }
     
-        instance.objectWillChange.send()
+        DispatchQueue.main.async {
+            instance.objectWillChange.send()
+        }
     }
    
    public func subscribe(_ instance: OcaRoot) async {
@@ -257,7 +259,9 @@ public struct OcaProperty<Value: Codable>: Codable, OcaPropertyChangeEventNotifi
 
     public func refresh(_ instance: OcaRoot) async {
         subject.send(.initial)
-        instance.objectWillChange.send()
+        DispatchQueue.main.async {
+            instance.objectWillChange.send()
+        }
     }
     
     func onEvent(_ instance: OcaRoot, _ eventData: Ocp1EventData) throws {
@@ -270,7 +274,9 @@ public struct OcaProperty<Value: Codable>: Codable, OcaPropertyChangeEventNotifi
         // TODO: how to handle items being deleted
         if .currentChanged == eventData.changeType {
             self.subject.send(.success(eventData.propertyValue))
-            instance.objectWillChange.send()
+            DispatchQueue.main.async {
+                instance.objectWillChange.send()
+            }
         } else {
             throw Ocp1Error.unhandledEvent
         }
