14c6a48e58d5bae9834a7405fb9a18be4932d044
Luke Howard <lukeh@padl.com>
2023-07-09 22:22:05 +1000

    don't use chain() as it requires channel ordering

--- Sources/SwiftOCAUI/Views/Discovery/BonjourDiscoveryView.swift
+++ Sources/SwiftOCAUI/Views/Discovery/BonjourDiscoveryView.swift
@@ -46,7 +46,19 @@ public struct OcaBonjourDiscoveryView: View {
             }
         }
         .task {
-            for await result in chain(udpBrowser.channel, tcpBrowser.channel) {
+            for await result in udpBrowser.channel {
+                switch result {
+                case let .didFind(netService):
+                    services.append(netService)
+                case let .didRemove(netService):
+                    services.removeAll(where: { $0 == netService })
+                case let .didNotSearch(error):
+                    debugPrint("OcaBonjourDiscoveryView: \(error)")
+                }
+            }
+        }
+        .task {
+            for await result in tcpBrowser.channel {
                 switch result {
                 case let .didFind(netService):
                     services.append(netService)
