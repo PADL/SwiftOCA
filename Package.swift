4f5ab33310080db89cbd45219bd42be61a1b0174
Luke Howard <lukeh@padl.com>
2023-07-18 12:49:54 +0200

    reformat

--- Package.swift
+++ Package.swift
@@ -7,14 +7,16 @@ let package = Package(
     name: "SwiftOCA",
     platforms: [
         // specify each minimum deployment requirement,
-        //otherwise the platform default minimum is used.
-       .macOS(.v10_15),
+        // otherwise the platform default minimum is used.
+        .macOS(.v10_15),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries produced by a package, and make them
+        // visible to other packages.
         .library(
             name: "SwiftOCA",
-            targets: ["SwiftOCA"]),
+            targets: ["SwiftOCA"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,16 +26,19 @@ let package = Package(
         .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.14.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets are the basic building blocks of a package. A target can define a module or a
+        // test suite.
+        // Targets can depend on other targets in this package, and on products in packages which
+        // this package depends on.
         .target(
             name: "SwiftOCA",
             dependencies: [
-		"AsyncExtensions",
-		"OpenCombine",
-		.product(name: "BinaryCoder", package: "swift-binary-coder"),
-		.product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
-	    ]),
+                "AsyncExtensions",
+                "OpenCombine",
+                .product(name: "BinaryCoder", package: "swift-binary-coder"),
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            ]
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
