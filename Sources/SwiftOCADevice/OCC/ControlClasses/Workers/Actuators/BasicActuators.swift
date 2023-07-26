2f11cfce3dfb3c0fb69e0fc4a54ff9e0267d3e0e
Luke Howard <lukeh@padl.com>
2023-07-26 13:52:08 +0200

    basic actuators

--- Sources/SwiftOCADevice/OCC/ControlClasses/Workers/Actuators/BasicActuators.swift
+++ Sources/SwiftOCADevice/OCC/ControlClasses/Workers/Actuators/BasicActuators.swift
@@ -17,6 +17,16 @@
 import Foundation
 import SwiftOCA
 
+public class OcaGenericBasicActuator<T: Codable & Comparable & Numeric>: OcaActuator {
+    @OcaBoundedDeviceProperty(
+        wrappedValue: OcaBoundedPropertyValue<T>(value: 0, in: 0...1),
+        propertyID: OcaPropertyID("5.1"),
+        getMethodID: OcaMethodID("5.1"),
+        setMethodID: OcaMethodID("5.2")
+    )
+    public var setting: OcaBoundedPropertyValue<T>
+}
+
 open class OcaBasicActuator: OcaActuator {
     override public class var classID: OcaClassID { OcaClassID("1.1.1.1") }
 }
@@ -30,4 +40,55 @@ open class OcaBooleanActuator: OcaActuator {
         setMethodID: OcaMethodID("5.2")
     )
     public var setting = false
+}
+
+public class OcaInt8Actuator: OcaGenericBasicActuator<OcaInt8> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.2") }
+}
+
+public class OcaInt16Actuator: OcaGenericBasicActuator<OcaInt16> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.3") }
+}
+
+public class OcaInt32Actuator: OcaGenericBasicActuator<OcaInt32> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.4") }
+}
+
+public class OcaInt64Actuator: OcaGenericBasicActuator<OcaInt64> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.5") }
+}
+
+public class OcaUint8Actuator: OcaGenericBasicActuator<OcaUint8> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.6") }
+}
+
+public class OcaUint16Actuator: OcaGenericBasicActuator<OcaUint16> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.7") }
+}
+
+public class OcaUint32Actuator: OcaGenericBasicActuator<OcaUint32> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.8") }
+}
+
+public class OcaUint64Actuator: OcaGenericBasicActuator<OcaUint64> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.9") }
+}
+
+public class OcaFloat32Actuator: OcaGenericBasicActuator<OcaFloat32> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.10") }
+}
+
+public class OcaFloat64Actuator: OcaGenericBasicActuator<OcaFloat64> {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.11") }
+}
+
+public class OcaStringActuator: OcaActuator {
+    override public class var classID: OcaClassID { OcaClassID("1.1.1.1.12") }
+
+    @OcaDeviceProperty(
+        propertyID: OcaPropertyID("5.1"),
+        getMethodID: OcaMethodID("5.1"),
+        setMethodID: OcaMethodID("5.2")
+    )
+    var setting = ""
 }
