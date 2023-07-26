85750db97fdcf9e22085872285bef8d05d9a14b4
Luke Howard <lukeh@padl.com>
2023-07-26 11:39:57 +0200

    cleanup

--- Examples/OCADevice/DeviceApp.swift
+++ Examples/OCADevice/DeviceApp.swift
@@ -14,36 +14,10 @@
 // limitations under the License.
 //
 
+import FlyingSocks
 import Foundation
 import SwiftOCA
 import SwiftOCADevice
-
-#if os(Linux)
-private var localhost = sockaddr_in(
-    sin_family: sa_family_t(AF_INET),
-    sin_port: in_port_t(65000).bigEndian,
-    sin_addr: in_addr(s_addr: INADDR_ANY),
-    sin_zero: (0, 0, 0, 0, 0, 0, 0, 0)
-)
-#else
-private var localhost = sockaddr_in(
-    sin_len: UInt8(MemoryLayout<sockaddr_in>.size),
-    sin_family: sa_family_t(AF_INET),
-    sin_port: in_port_t(65000).bigEndian,
-    sin_addr: in_addr(s_addr: INADDR_ANY),
-    sin_zero: (0, 0, 0, 0, 0, 0, 0, 0)
-)
-#endif
-
-class TestActuator: SwiftOCADevice.OcaBooleanActuator {
-    override public func handleCommand(
-        _ command: Ocp1Command,
-        from controller: AES70OCP1Controller
-    ) async throws -> Ocp1Response {
-        debugPrint("got command \(command) from controller \(controller)")
-        return try await super.handleCommand(command, from: controller)
-    }
-}
 
 @main
 public enum DeviceApp {
@@ -52,7 +26,9 @@ public enum DeviceApp {
     public static func main() async throws {
         var device: AES70OCP1Device!
 
-        withUnsafePointer(to: &localhost) {
+        var localAddress = sockaddr_in.inet(port: 65000)
+
+        withUnsafePointer(to: &localAddress) {
             $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { cSockAddr in
                 device = AES70OCP1Device(address: cSockAddr)
             }
