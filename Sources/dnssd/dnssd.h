ca8f271b1041edf283e4c6c4efc768ea12fe8b37
Luke Howard <lukeh@padl.com>
2023-09-22 16:50:11 +1000

    remove DNSServiceRegisterReplyBlock

--- Sources/dnssd/dnssd.h
+++ Sources/dnssd/dnssd.h
@@ -1,18 +1,3 @@
 #pragma once
 
 #include <dns_sd.h>
-
-#ifndef __APPLE__
-#include <Block/Block.h>
-#endif
-
-typedef void (^DNSServiceRegisterReplyBlock)
-(
-    DNSServiceRef sdRef,
-    DNSServiceFlags flags,
-    DNSServiceErrorType errorCode,
-    const char *name,
-    const char *regtype,
-    const char *domain
-);
-
