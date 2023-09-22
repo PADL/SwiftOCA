7e683067e1773f9e2b1ee0d1e461c967d2836675
Luke Howard <lukeh@padl.com>
2023-09-22 16:44:47 +1000

    add .apt provider

--- Package.swift
+++ Package.swift
@@ -6,13 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftOCA",
     platforms: [
-        // specify each minimum deployment requirement,
-        // otherwise the platform default minimum is used.
         .macOS(.v10_15),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them
-        // visible to other packages.
         .library(
             name: "SwiftOCA",
             targets: ["SwiftOCA"]
@@ -23,7 +19,6 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
         .package(url: "https://github.com/PADL/swift-binary-coder", branch: "inferno"),
         .package(url: "https://github.com/lhoward/AsyncExtensions", branch: "linux"),
@@ -31,12 +26,9 @@ let package = Package(
         .package(url: "https://github.com/PADL/IORingSwift", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a
-        // test suite.
-        // Targets can depend on other targets in this package, and on products in packages which
-        // this package depends on.
         .systemLibrary(
-            name: "dnssd"
+            name: "dnssd",
+            providers: [.apt(["libavahi-compat-libdnssd-dev"])]
         ),
         .target(
             name: "SwiftOCA",
