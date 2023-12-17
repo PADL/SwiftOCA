22ec08d1871e748ad6c4e8162c86c413e7d134a5
Luke Howard <lukeh@padl.com>
2023-12-18 10:10:15 +1100

    implement Ocp1SystemInterfaceParameters

--- Sources/SwiftOCA/OCC/ControlDataTypes/NetworkDataTypes.swift
+++ Sources/SwiftOCA/OCC/ControlDataTypes/NetworkDataTypes.swift
@@ -41,17 +41,116 @@ public enum OcaNetworkControlProtocol: OcaUint8, Codable, Sendable {
     case ocp03 = 3 // JSON
 }
 
+public enum Ocp1IPParametersType: OcaUint8, Codable, Sendable {
+    case unknown = 0
+    case linkLocal = 1
+    case dhcp = 2
+    case `static` = 3
+}
+
+public struct Ocp1SystemInterfaceParameters: Codable, Sendable {
+    public typealias EUI48 = (OcaUint8, OcaUint8, OcaUint8, OcaUint8, OcaUint8, OcaUint8)
+
+    public let version: OcaUint16 // 1
+    public let hostname: OcaString
+    public let interfaceIndex: OcaUint32
+    public let subnetMaskLength: OcaUint8
+    public let defaultGateway: OcaString
+    public let dnsServers: OcaString // comma-separated
+    public let linkUp: OcaBoolean
+    public let adapterSpeed: OcaUint64
+    public let parametersType: Ocp1IPParametersType
+    public let macAddress: EUI48
+    public let linkType: OcaNetworkLinkType
+
+    public init(
+        version: OcaUint16,
+        hostname: OcaString,
+        interfaceIndex: OcaUint32,
+        subnetMaskLength: OcaUint8,
+        defaultGateway: OcaString,
+        dnsServers: OcaString,
+        linkUp: OcaBoolean,
+        adapterSpeed: OcaUint64,
+        parametersType: Ocp1IPParametersType,
+        macAddress: (OcaUint8, OcaUint8, OcaUint8, OcaUint8, OcaUint8, OcaUint8),
+        linkType: OcaNetworkLinkType
+    ) {
+        self.version = version
+        self.hostname = hostname
+        self.interfaceIndex = interfaceIndex
+        self.subnetMaskLength = subnetMaskLength
+        self.defaultGateway = defaultGateway
+        self.dnsServers = dnsServers
+        self.linkUp = linkUp
+        self.adapterSpeed = adapterSpeed
+        self.parametersType = parametersType
+        self.macAddress = macAddress
+        self.linkType = linkType
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        version = try container.decode(OcaUint16.self, forKey: .version)
+        hostname = try container.decode(OcaString.self, forKey: .hostname)
+        interfaceIndex = try container.decode(OcaUint32.self, forKey: .interfaceIndex)
+        subnetMaskLength = try container.decode(OcaUint8.self, forKey: .subnetMaskLength)
+        defaultGateway = try container.decode(OcaString.self, forKey: .defaultGateway)
+        dnsServers = try container.decode(OcaString.self, forKey: .dnsServers)
+        linkUp = try container.decode(OcaBoolean.self, forKey: .linkUp)
+        adapterSpeed = try container.decode(OcaUint64.self, forKey: .adapterSpeed)
+        parametersType = try container.decode(Ocp1IPParametersType.self, forKey: .parametersType)
+        var macAddressContainer = try container.nestedUnkeyedContainer(forKey: .macAddress)
+        var macAddress: EUI48 = (0, 0, 0, 0, 0, 0)
+        macAddress.0 = try macAddressContainer.decode(OcaUint8.self)
+        macAddress.1 = try macAddressContainer.decode(OcaUint8.self)
+        macAddress.2 = try macAddressContainer.decode(OcaUint8.self)
+        macAddress.3 = try macAddressContainer.decode(OcaUint8.self)
+        macAddress.4 = try macAddressContainer.decode(OcaUint8.self)
+        macAddress.5 = try macAddressContainer.decode(OcaUint8.self)
+        self.macAddress = macAddress
+        linkType = try container.decode(OcaNetworkLinkType.self, forKey: .linkType)
+    }
+
+    public enum CodingKeys: CodingKey {
+        case version
+        case hostname
+        case interfaceIndex
+        case subnetMaskLength
+        case defaultGateway
+        case dnsServers
+        case linkUp
+        case adapterSpeed
+        case parametersType
+        case macAddress
+        case linkType
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(version, forKey: .version)
+        try container.encode(hostname, forKey: .hostname)
+        try container.encode(interfaceIndex, forKey: .interfaceIndex)
+        try container.encode(subnetMaskLength, forKey: .subnetMaskLength)
+        try container.encode(defaultGateway, forKey: .defaultGateway)
+        try container.encode(dnsServers, forKey: .dnsServers)
+        try container.encode(linkUp, forKey: .linkUp)
+        try container.encode(adapterSpeed, forKey: .adapterSpeed)
+        try container.encode(parametersType, forKey: .parametersType)
+        var macAddressContainer = container.nestedUnkeyedContainer(forKey: .macAddress)
+        try macAddressContainer.encode(macAddress.0)
+        try macAddressContainer.encode(macAddress.1)
+        try macAddressContainer.encode(macAddress.2)
+        try macAddressContainer.encode(macAddress.3)
+        try macAddressContainer.encode(macAddress.4)
+        try macAddressContainer.encode(macAddress.5)
+        try container.encode(linkType, forKey: .linkType)
+    }
+}
+
 public struct OcaNetworkSystemInterfaceDescriptor: Codable, Sendable {
-    let systemInterfaceParameters: OcaBlob
-    let myNetworkAddress: OcaNetworkAddress
+    public let systemInterfaceParameters: OcaBlob
+    public let myNetworkAddress: OcaNetworkAddress
 }
 
 public typealias OcaAdaptationIdentifier = OcaString
-
-/*
- public struct OcaNetworkAdvertisingMechanism: Codable, Sendable {
-     let service: OcaNetworkAdvertisingService
-     let parameters: OcaParameterRecord
-     let networkInterfaceAssignment: OcaNetworkInterfaceAssignment
- }
- */
