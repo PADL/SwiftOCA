7486980107f13a1e4f6fa9fdb11ae7f0b5d02fe9
Luke Howard <lukeh@padl.com>
2023-12-19 08:10:08 +1100

    OcaCodingManager

---
+++ Sources/SwiftOCA/OCC/ControlClasses/Managers/CodingManager.swift
@@ -0,0 +1,36 @@
+//
+// Copyright (c) 2023 PADL Software Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+open class OcaCodingManager: OcaManager {
+    override public class var classID: OcaClassID { OcaClassID("1.3.12") }
+    override public class var classVersion: OcaClassVersionNumber { 3 }
+
+    @OcaProperty(
+        propertyID: OcaPropertyID("3.1"),
+        getMethodID: OcaMethodID("3.1")
+    )
+    public var availableEncodingSchemes: OcaProperty<[OcaMediaCodingSchemeID: OcaString]>.State
+
+    @OcaProperty(
+        propertyID: OcaPropertyID("3.2"),
+        getMethodID: OcaMethodID("3.2")
+    )
+    public var availableDecodingSchemes: OcaProperty<[OcaMediaCodingSchemeID: OcaString]>.State
+
+    public convenience init() {
+        self.init(objectNumber: OcaCodingManagerONo)
+    }
+}
---
+++ Sources/SwiftOCADevice/OCC/ControlClasses/Managers/CodingManager.swift
@@ -0,0 +1,43 @@
+//
+// Copyright (c) 2023 PADL Software Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import SwiftOCA
+
+open class OcaCodingManager: OcaManager {
+    override open class var classID: OcaClassID { OcaClassID("1.3.12") }
+    override open class var classVersion: OcaClassVersionNumber { 3 }
+
+    @OcaDeviceProperty(
+        propertyID: OcaPropertyID("3.1"),
+        getMethodID: OcaMethodID("3.1")
+    )
+    public var availableEncodingSchemes: [OcaMediaCodingSchemeID: OcaString] = [:]
+
+    @OcaDeviceProperty(
+        propertyID: OcaPropertyID("3.2"),
+        getMethodID: OcaMethodID("3.2")
+    )
+    public var availableDecodingSchemes: [OcaMediaCodingSchemeID: OcaString] = [:]
+
+    public convenience init(deviceDelegate: AES70Device? = nil) async throws {
+        try await self.init(
+            objectNumber: OcaCodingManagerONo,
+            role: "Coding Manager",
+            deviceDelegate: deviceDelegate,
+            addToRootBlock: true
+        )
+    }
+}
