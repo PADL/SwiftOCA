db45ab25db3fcb2048891ed86ec615e7cc12eaa4
Luke Howard <lukeh@padl.com>
2024-01-13 09:35:32 +1100

    make proxy init public

--- Sources/SwiftOCADevice/AES70/Backend/DatagramProxy/DatagramProxyController.swift
+++ Sources/SwiftOCADevice/AES70/Backend/DatagramProxy/DatagramProxyController.swift
@@ -30,7 +30,7 @@ actor DatagramProxyController<T: Equatable & Hashable>: AES70ControllerPrivate {
         AsyncEmptySequence<ControllerMessage>().eraseToAnyAsyncSequence()
     }
 
-    init(with peerID: T, endpoint: DatagramProxyDeviceEndpoint<T>) {
+    public init(with peerID: T, endpoint: DatagramProxyDeviceEndpoint<T>) {
         self.peerID = peerID
         self.endpoint = endpoint
     }
