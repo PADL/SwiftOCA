fd3194927f6fbd4c87479669a666bd865fbec4c9
Luke Howard <lukeh@padl.com>
2024-01-20 09:13:14 +1100

    separate common and platform products and dependencies

--- Package.swift
+++ Package.swift
@@ -15,14 +15,15 @@ if EnableASAN {
     ASANLinkerSettings.append(LinkerSetting.linkedLibrary("asan"))
 }
 
-let PlatformPackages: [Package.Dependency]
-let PlatformDependencies: [Target.Dependency]
+let PlatformPackageDependencies: [Package.Dependency]
+let PlatformTargetDependencies: [Target.Dependency]
+let PlatformProducts: [Product]
 let PlatformTargets: [Target]
 
 #if os(Linux)
-PlatformPackages = [.package(url: "https://github.com/PADL/IORingSwift", branch: "main")]
+PlatformPackageDependencies = [.package(url: "https://github.com/PADL/IORingSwift", branch: "main")]
 
-PlatformDependencies = [
+PlatformTargetDependencies = [
     "dnssd",
     .product(
         name: "IORing",
@@ -41,15 +42,18 @@ PlatformDependencies = [
     ),
 ]
 
+PlatformProducts = []
 PlatformTargets = []
-#else
-PlatformPackages = [.package(url: "https://github.com/swhitty/FlyingFox", branch: "main"),
-                    .package(
-                        url: "https://github.com/spacenation/swiftui-sliders.git",
-                        from: "2.1.0"
-                    )]
+#elseif os(macOS) || os(iOS)
+PlatformPackageDependencies = [
+    .package(url: "https://github.com/swhitty/FlyingFox", branch: "main"),
+    .package(
+        url: "https://github.com/spacenation/swiftui-sliders.git",
+        from: "2.1.0"
+    ),
+]
 
-PlatformDependencies = [
+PlatformTargetDependencies = [
     .product(
         name: "FlyingFox",
         package: "FlyingFox",
@@ -59,6 +63,13 @@ PlatformDependencies = [
         name: "FlyingSocks",
         package: "FlyingFox",
         condition: .when(platforms: [.macOS, .iOS])
+    ),
+]
+
+PlatformProducts = [
+    .library(
+        name: "SwiftOCAUI",
+        targets: ["SwiftOCAUI"]
     ),
 ]
 
@@ -91,78 +102,90 @@ PlatformTargets = [
         linkerSettings: [] + ASANLinkerSettings
     ),
 ]
+#else
+PlatformPackageDependencies = []
+PlatformTargetDependencies = []
+PlatformProducts = []
+PlatformTargets = []
 #endif
+
+let CommonPackageDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
+    .package(url: "https://github.com/apple/swift-log", branch: "main"),
+    .package(url: "https://github.com/lhoward/AsyncExtensions", branch: "linux"),
+    .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
+]
+
+let CommonProducts: [Product] = [
+    .library(
+        name: "SwiftOCA",
+        targets: ["SwiftOCA"]
+    ),
+    .library(
+        name: "SwiftOCADevice",
+        targets: ["SwiftOCADevice"]
+    ),
+]
+
+let CommonTargets: [Target] = [
+    .systemLibrary(
+        name: "dnssd",
+        providers: [.apt(["libavahi-compat-libdnssd-dev"])]
+    ),
+    .target(
+        name: "SwiftOCA",
+        dependencies: [
+            "AsyncExtensions",
+            "AnyCodable",
+            .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            .product(name: "Logging", package: "swift-log"),
+        ] + PlatformTargetDependencies,
+        swiftSettings: [
+            .enableExperimentalFeature("StrictConcurrency"),
+        ]
+    ),
+    .target(
+        name: "SwiftOCADevice",
+        dependencies: [
+            "SwiftOCA",
+            .product(name: "Logging", package: "swift-log"),
+        ] + PlatformTargetDependencies,
+        swiftSettings: [
+            .enableExperimentalFeature("StrictConcurrency"),
+        ]
+    ),
+    .executableTarget(
+        name: "OCADevice",
+        dependencies: [
+            "SwiftOCADevice",
+        ],
+        path: "Examples/OCADevice",
+        swiftSettings: [
+            .unsafeFlags(ASANSwiftFlags),
+        ],
+        linkerSettings: [] + ASANLinkerSettings
+
+    ),
+    .testTarget(
+        name: "SwiftOCADeviceTests",
+        dependencies: [
+            .target(name: "SwiftOCADevice"),
+        ],
+        swiftSettings: [
+            .unsafeFlags(ASANSwiftFlags),
+        ],
+        linkerSettings: [] + ASANLinkerSettings
+    ),
+]
 
 let package = Package(
     name: "SwiftOCA",
     platforms: [
         .macOS(.v13),
+        .iOS(.v17),
     ],
-    products: [
-        .library(
-            name: "SwiftOCA",
-            targets: ["SwiftOCA"]
-        ),
-        .library(
-            name: "SwiftOCADevice",
-            targets: ["SwiftOCADevice"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
-        .package(url: "https://github.com/apple/swift-log", branch: "main"),
-        .package(url: "https://github.com/lhoward/AsyncExtensions", branch: "linux"),
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
-    ] + PlatformPackages,
-    targets: [
-        .systemLibrary(
-            name: "dnssd",
-            providers: [.apt(["libavahi-compat-libdnssd-dev"])]
-        ),
-        .target(
-            name: "SwiftOCA",
-            dependencies: [
-                "AsyncExtensions",
-                "AnyCodable",
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
-                .product(name: "Logging", package: "swift-log"),
-            ] + PlatformDependencies,
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency"),
-            ]
-        ),
-        .target(
-            name: "SwiftOCADevice",
-            dependencies: [
-                "SwiftOCA",
-                .product(name: "Logging", package: "swift-log"),
-            ] + PlatformDependencies,
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency"),
-            ]
-        ),
-        .executableTarget(
-            name: "OCADevice",
-            dependencies: [
-                "SwiftOCADevice",
-            ],
-            path: "Examples/OCADevice",
-            swiftSettings: [
-                .unsafeFlags(ASANSwiftFlags),
-            ],
-            linkerSettings: [] + ASANLinkerSettings
-
-        ),
-        .testTarget(
-            name: "SwiftOCADeviceTests",
-            dependencies: [
-                .target(name: "SwiftOCADevice"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(ASANSwiftFlags),
-            ],
-            linkerSettings: [] + ASANLinkerSettings
-        ),
-    ],
+    products: CommonProducts + PlatformProducts,
+    dependencies: CommonPackageDependencies + PlatformPackageDependencies,
+    targets: CommonTargets + PlatformTargets,
     swiftLanguageVersions: [.v5]
 )
