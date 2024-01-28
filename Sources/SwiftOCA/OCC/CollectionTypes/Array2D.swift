PADL/SwiftOCA

commit 58f694d770e5b2d42a56d46e8537b2748d884d24

emit matrix and block member changed notifications
this also fixes a race condition introduced when we made matrix and block
member accessors actor-safe

Sources/SwiftOCADevice/OCC/ControlClasses/Workers/BlocksAndMatrices/Block.swift
@@ -26,13 +26,27 @@ open class OcaBlock<ActionObject: OcaRoot>: OcaWorker {
     )
     public var type: OcaONo = OcaInvalidONo
 
-    @OcaDeviceProperty(
-        propertyID: OcaPropertyID("3.2")
-    )
-    public var actionObjects = [ActionObject]()
+    public private(set) var actionObjects = [ActionObject]()
+
+    private func notifySubscribers(
+        object: ActionObject,
+        changeType: OcaPropertyChangeType
+    ) async throws {
+        let event = OcaEvent(emitterONo: object.objectNumber, eventID: OcaPropertyChangedEventID)
+        let parameters = OcaPropertyChangedEventData<ActionObject>(
+            propertyID: OcaPropertyID("3.2"),
+            propertyValue: object,
+            changeType: changeType
+        )
+
+        try await deviceDelegate?.notifySubscribers(
+            event,
+            parameters: parameters
+        )
+    }
 
     @OcaDevice
-    open func add(actionObject object: ActionObject) throws {
+    open func add(actionObject object: ActionObject) async throws {
         guard object.objectNumber != OcaInvalidONo else {
             throw Ocp1Error.status(.badONo)
         }
@@ -57,10 +71,11 @@ open class OcaBlock<ActionObject: OcaRoot>: OcaWorker {
         }
 
         actionObjects.append(object)
+        try? await notifySubscribers(object: object, changeType: .itemAdded)
     }
 
     @OcaDevice
-    open func remove(actionObject object: ActionObject) throws {
+    open func remove(actionObject object: ActionObject) async throws {
         if object.objectNumber == OcaInvalidONo {
             throw Ocp1Error.status(.badONo)
         }
@@ -76,6 +91,7 @@ open class OcaBlock<ActionObject: OcaRoot>: OcaWorker {
         }
 
         actionObjects.remove(at: index)
+        try? await notifySubscribers(object: object, changeType: .itemDeleted)
     }
 
     @OcaDevice

Sources/SwiftOCADevice/OCC/ControlClasses/Workers/BlocksAndMatrices/Matrix.swift
@@ -21,8 +21,13 @@ private let OcaMatrixWildcardCoordinate: OcaUint16 = 0xFFFF
 open class OcaMatrix<Member: OcaRoot>: OcaWorker {
     override open class var classID: OcaClassID { OcaClassID("1.1.5") }
 
+    @OcaDevice
     public private(set) var members: OcaArray2D<Member?>
+
+    @OcaDevice
     public private(set) var proxy: Proxy<Member>!
+
+    @OcaDevice
     private var lockStatePriorToSetCurrentXY: LockState?
 
     public init(
@@ -80,7 +85,7 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
             matrix?.objectNumber ?? OcaInvalidONo
         }
 
-        actor CommandBox {
+        fileprivate actor CommandBox {
             var response: Ocp1Response?
             var lastStatus: OcaStatus?
 
@@ -209,21 +214,64 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
         y: OcaMatrixWildcardCoordinate
     )
 
-    @OcaDevice
-    open func add(member object: Member, at coordinate: OcaVector2D<OcaMatrixCoordinate>) throws {
-        precondition(object != self)
-        guard coordinate.isValid(in: self) else {
-            throw Ocp1Error.status(.parameterOutOfRange)
-        }
-        members[Int(coordinate.x), Int(coordinate.y)] = object
+    private func notifySubscribers(
+        object: Member,
+        changeType: OcaPropertyChangeType
+    ) async throws {
+        let event = OcaEvent(emitterONo: object.objectNumber, eventID: OcaPropertyChangedEventID)
+        let parameters = OcaPropertyChangedEventData<Member>(
+            propertyID: OcaPropertyID("3.5"),
+            propertyValue: object,
+            changeType: changeType
+        )
+
+        try await deviceDelegate?.notifySubscribers(
+            event,
+            parameters: parameters
+        )
     }
 
     @OcaDevice
-    open func remove(coordinate: OcaVector2D<OcaMatrixCoordinate>) throws {
-        guard coordinate.isValid(in: self) else {
+    private func isValid(coordinate: OcaVector2D<OcaMatrixCoordinate>) async -> Bool {
+        coordinate.x < members.nX && coordinate.y < members.nY
+    }
+
+    @OcaDevice
+    open func add(
+        member object: Member,
+        at coordinate: OcaVector2D<OcaMatrixCoordinate>
+    ) async throws {
+        precondition(object != self)
+        guard await isValid(coordinate: coordinate) else {
             throw Ocp1Error.status(.parameterOutOfRange)
         }
+        members[Int(coordinate.x), Int(coordinate.y)] = object
+        try? await notifySubscribers(object: object, changeType: .itemAdded)
+    }
+
+    @OcaDevice
+    open func remove(coordinate: OcaVector2D<OcaMatrixCoordinate>) async throws {
+        guard await isValid(coordinate: coordinate) else {
+            throw Ocp1Error.status(.parameterOutOfRange)
+        }
+        guard let oldMember = members[Int(coordinate.x), Int(coordinate.y)] else {
+            throw Ocp1Error.status(.parameterError)
+        }
         members[Int(coordinate.x), Int(coordinate.y)] = nil
+        try? await notifySubscribers(object: oldMember, changeType: .itemDeleted)
+    }
+
+    @OcaDevice
+    open func set(
+        member object: Member,
+        at coordinate: OcaVector2D<OcaMatrixCoordinate>
+    ) async throws {
+        precondition(object != self)
+        guard await isValid(coordinate: coordinate) else {
+            throw Ocp1Error.status(.parameterOutOfRange)
+        }
+        members[Int(coordinate.x), Int(coordinate.y)] = object
+        try? await notifySubscribers(object: object, changeType: .itemChanged)
     }
 
     @OcaDevice
@@ -283,6 +331,7 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
         case OcaMethodID("3.3"):
             try decodeNullCommand(command)
             try await ensureReadable(by: controller, command: command)
+            let members = await members
             let size = OcaVector2D<OcaMatrixCoordinate>(
                 x: OcaMatrixCoordinate(members.nX),
                 y: OcaMatrixCoordinate(members.nY)
@@ -299,38 +348,38 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
         case OcaMethodID("3.5"):
             try decodeNullCommand(command)
             try await ensureReadable(by: controller, command: command)
-            let members = members
+            let members = await members
                 .map(defaultValue: OcaInvalidONo) { $0?.objectNumber ?? OcaInvalidONo }
             return try encodeResponse(members)
         case OcaMethodID("3.7"):
             let coordinates: OcaVector2D<OcaMatrixCoordinate> = try decodeCommand(command)
             try await ensureReadable(by: controller, command: command)
-            let objectNumber = members[Int(coordinates.x), Int(coordinates.y)]?
+            let objectNumber = await members[Int(coordinates.x), Int(coordinates.y)]?
                 .objectNumber ?? OcaInvalidONo
             return try encodeResponse(objectNumber)
         case OcaMethodID("3.8"):
             let parameters: SwiftOCA.OcaMatrix.SetMemberParameters = try decodeCommand(command)
             try await ensureWritable(by: controller, command: command)
+            let members = await members
             guard parameters.x < members.nX, parameters.y < members.nY else {
                 throw Ocp1Error.status(.parameterOutOfRange)
             }
-            let object: Member?
             if parameters.memberONo == OcaInvalidONo {
-                object = nil
-            } else {
-                object = await deviceDelegate?.objects[parameters.memberONo] as? Member
-                if object == nil {
-                    throw Ocp1Error.status(.badONo)
-                }
+                throw Ocp1Error.status(.badONo)
             }
-            members[Int(parameters.x), Int(parameters.y)] = object
+            let object = await deviceDelegate?.objects[parameters.memberONo] as? Member
+            guard let object else {
+                throw Ocp1Error.status(.badONo)
+            }
+            try await set(member: object, at: OcaVector2D(x: parameters.x, y: parameters.y))
         case OcaMethodID("3.9"):
             try decodeNullCommand(command)
             try await ensureReadable(by: controller, command: command)
-            return try encodeResponse(proxy.objectNumber)
+            return try await encodeResponse(proxy.objectNumber)
         case OcaMethodID("3.2"):
             let coordinates: OcaVector2D<OcaMatrixCoordinate> = try decodeCommand(command)
             try await ensureWritable(by: controller, command: command)
+            let members = await members
             guard coordinates.x < members.nX || coordinates.x == OcaMatrixWildcardCoordinate,
                   coordinates.y < members.nY || coordinates.y == OcaMatrixWildcardCoordinate
             else {
@@ -353,12 +402,5 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
 
     override public var isContainer: Bool {
         true
-    }
-}
-
-extension OcaVector2D where T == OcaMatrixCoordinate {
-    func isValid<Member: OcaRoot>(in matrix: OcaMatrix<Member>) -> Bool {
-        x != OcaMatrixWildcardCoordinate && x < matrix.members.nX &&
-            y != OcaMatrixWildcardCoordinate && y < matrix.members.nY
     }
 }

Tests/SwiftOCADeviceTests/SwiftOCADeviceTests.swift
@@ -271,8 +271,9 @@ final class SwiftOCADeviceTests: XCTestCase {
                 addToRootBlock: true
             )
 
-        for x in 0..<matrix.members.nX {
-            for y in 0..<matrix.members.nY {
+        let matrixMembers = await matrix.members
+        for x in 0..<matrixMembers.nX {
+            for y in 0..<matrixMembers.nY {
                 let coordinate = OcaVector2D(x: OcaMatrixCoordinate(x), y: OcaMatrixCoordinate(y))
                 let actuator = try await MyBooleanActuator(
                     role: "Actuator \(x),\(y)",

Examples/OCADevice/DeviceApp.swift
@@ -78,8 +78,9 @@ public enum DeviceApp {
         let block = try await SwiftOCADevice
             .OcaBlock<SwiftOCADevice.OcaWorker>(role: "Block", deviceDelegate: device)
 
-        for x in 0..<matrix.members.nX {
-            for y in 0..<matrix.members.nY {
+        let members = await matrix.members
+        for x in 0..<members.nX {
+            for y in 0..<members.nY {
                 let coordinate = OcaVector2D(x: OcaMatrixCoordinate(x), y: OcaMatrixCoordinate(y))
                 let actuator = try await MyBooleanActuator(
                     role: "Actuator(\(x),\(y))",

Sources/SwiftOCADevice/OCC/ControlClasses/PortsRepresentable.swift
@@ -89,7 +89,7 @@ public extension SwiftOCADevice.OcaBlock where ActionObject: OcaPortsRepresentab
             _ = try await add(signalPath: signalPath)
 
             if addToBlock, outputs[i] != self, !actionObjects.contains(outputs[i]) {
-                try add(actionObject: outputs[i])
+                try await add(actionObject: outputs[i])
             }
         }
     }

commit 37295b29892a0a00ea01e9892a9b0c7cf6429176

refactor JSON serialization code
Fix a regression introduced in 58f694d7. OcaBlock and OcaMatrix members need to
be serialized explicitly now that they are not contained in property wrappers.
Additionally, JSON encode OcaArray2 as a multi-dimensional array. Add tests.

Sources/SwiftOCA/OCC/CollectionTypes/Array2D.swift
@@ -72,23 +72,56 @@ public struct OcaArray2D<Element: Sendable>: Sendable {
 
 extension OcaArray2D: Codable where Element: Codable {
     public init(from decoder: Decoder) throws {
-        var container = try decoder.unkeyedContainer()
-        nX = Int(try container.decode(OcaUint16.self))
-        nY = Int(try container.decode(OcaUint16.self))
+        if decoder._isOcp1Decoder {
+            var container = try decoder.unkeyedContainer()
+            nX = Int(try container.decode(OcaUint16.self))
+            nY = Int(try container.decode(OcaUint16.self))
 
-        items = [Element]()
-        items.reserveCapacity(Int(nX * nY))
-        for index in 0..<count {
-            items.insert(try container.decode(Element.self), at: index)
+            items = [Element]()
+            items.reserveCapacity(Int(nX * nY))
+            for index in 0..<count {
+                items.insert(try container.decode(Element.self), at: index)
+            }
+        } else {
+            var container = try decoder.unkeyedContainer()
+            let items = try container.decode([[Element]].self)
+            var columnCount: Int?
+
+            guard items.allSatisfy({ row in
+                if let columnCount {
+                    return row.count == columnCount
+                } else {
+                    columnCount = row.count
+                    return true
+                }
+            }) else {
+                throw DecodingError
+                    .dataCorrupted(DecodingError.Context(
+                        codingPath: decoder.codingPath,
+                        debugDescription: "OcaArray2D must have consistent dimensions"
+                    ))
+            }
+
+            nX = columnCount ?? 0
+            nY = items.count
+            self.items = items.reduce([], +)
         }
     }
 
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.unkeyedContainer()
-        try container.encode(OcaUint16(nX))
-        try container.encode(OcaUint16(nY))
-        for index in 0..<count {
-            try container.encode(items[index])
+        if encoder._isOcp1Encoder {
+            var container = encoder.unkeyedContainer()
+            try container.encode(OcaUint16(nX))
+            try container.encode(OcaUint16(nY))
+            for index in 0..<count {
+                try container.encode(items[index])
+            }
+        } else {
+            var columnContainer = encoder.unkeyedContainer()
+            for x in 0..<nX {
+                var rowContainer = columnContainer.nestedUnkeyedContainer()
+                try rowContainer.encode(contentsOf: items[(x * nY)..<(x * nY + nY)])
+            }
         }
     }
 }

Sources/SwiftOCADevice/OCC/ControlClasses/Root+JSON.swift
@@ -16,32 +16,12 @@
 
 import Foundation
 import SwiftOCA
+@_implementationOnly
+import AnyCodable
 
-private let objectNumberJSONKey = "_oNo"
-private let classIDJSONKey = "_classID"
-private let actionObjectsJSONKey = "_members"
-
-public extension OcaRoot {
-    var jsonObject: [String: Any] {
-        var dict = [String: Any]()
-
-        precondition(objectNumber != OcaInvalidONo)
-
-        guard self is OcaWorker else {
-            return [:]
-        }
-
-        dict[objectNumberJSONKey] = objectNumber
-        dict[classIDJSONKey] = Self.classID.description
-
-        for (_, propertyKeyPath) in allDevicePropertyKeyPaths {
-            let property = self[keyPath: propertyKeyPath] as! (any OcaDevicePropertyRepresentable)
-            dict[property.propertyID.description] = try? property.getJsonValue()
-        }
-
-        return dict
-    }
-}
+let objectNumberJSONKey = "_oNo"
+let classIDJSONKey = "_classID"
+let actionObjectsJSONKey = "_members"
 
 public extension OcaDevice {
     @discardableResult
@@ -76,5 +56,12 @@ public extension OcaDevice {
         }
 
         return object
+    }
+}
+
+extension JSONEncoder {
+    func reencodeAsValidJSONObject<T: Codable>(_ value: T) throws -> Any {
+        let data = try encode(value)
+        return try JSONDecoder().decode(AnyDecodable.self, from: data).value
     }
 }

Sources/SwiftOCADevice/OCC/ControlClasses/Root.swift
@@ -335,6 +335,26 @@ Sendable, OcaKeyPathMarkerProtocol {
             return false
         }
     }
+
+    public var jsonObject: [String: Any] {
+        var dict = [String: Any]()
+
+        precondition(objectNumber != OcaInvalidONo)
+
+        guard self is OcaWorker else {
+            return [:]
+        }
+
+        dict[objectNumberJSONKey] = objectNumber
+        dict[classIDJSONKey] = Self.classID.description
+
+        for (_, propertyKeyPath) in allDevicePropertyKeyPaths {
+            let property = self[keyPath: propertyKeyPath] as! (any OcaDevicePropertyRepresentable)
+            dict[property.propertyID.description] = try? property.getJsonValue()
+        }
+
+        return dict
+    }
 }
 
 extension OcaRoot: Equatable {

Sources/SwiftOCADevice/OCC/ControlClasses/Workers/BlocksAndMatrices/Block.swift
@@ -385,6 +385,12 @@ open class OcaBlock<ActionObject: OcaRoot>: OcaWorker {
     override public var isContainer: Bool {
         true
     }
+
+    override public var jsonObject: [String: Any] {
+        var jsonObject = super.jsonObject
+        jsonObject["3.2"] = actionObjects.map(\.jsonObject)
+        return jsonObject
+    }
 }
 
 public extension OcaRoot {

Sources/SwiftOCADevice/OCC/ControlClasses/Workers/BlocksAndMatrices/Matrix.swift
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+import Foundation
 import SwiftOCA
 
 private let OcaMatrixWildcardCoordinate: OcaUint16 = 0xFFFF
@@ -390,5 +391,12 @@ open class OcaMatrix<Member: OcaRoot>: OcaWorker {
 
     override public var isContainer: Bool {
         true
+    }
+
+    override public var jsonObject: [String: Any] {
+        var jsonObject = super.jsonObject
+        let membersJson = members.map(defaultValue: nil, \.?.objectNumber)
+        jsonObject["3.5"] = try? JSONEncoder().reencodeAsValidJSONObject(membersJson)
+        return jsonObject
     }
 }

Sources/SwiftOCADevice/OCC/PropertyTypes/DeviceProperty.swift
@@ -142,13 +142,10 @@ public struct OcaDeviceProperty<Value: Codable & Sendable>: OcaDevicePropertyRep
 
         if isNil(subject.value) {
             jsonValue = NSNull()
-        } else if let value = subject.value as? [OcaRoot] {
-            jsonValue = value.map(\.jsonObject)
-        } else if !JSONSerialization.isValidJSONObject(subject.value) {
-            let data = try JSONEncoder().encode(subject.value)
-            jsonValue = try JSONDecoder().decode(AnyDecodable.self, from: data).value
-        } else {
+        } else if JSONSerialization.isValidJSONObject(subject.value) {
             jsonValue = subject.value
+        } else {
+            jsonValue = try JSONEncoder().reencodeAsValidJSONObject(subject.value)
         }
 
         return jsonValue

SwiftOCA.xcodeproj/project.pbxproj
@@ -27,7 +27,6 @@
 		D32229D42ABC270900B56B6B /* Ocp1IORingController.swift in Sources */ = {isa = PBXBuildFile; fileRef = D32229CC2ABC270900B56B6B /* Ocp1IORingController.swift */; };
 		D32229D62ABD26E100B56B6B /* DeviceEndpointRegistrar.swift in Sources */ = {isa = PBXBuildFile; fileRef = D32229D52ABD26E100B56B6B /* DeviceEndpointRegistrar.swift */; };
 		D32C49DD2B5102A300D73201 /* Logging in Frameworks */ = {isa = PBXBuildFile; productRef = D32C49DC2B5102A300D73201 /* Logging */; };
-		D337713F2B48A829003BD47B /* Root+JSON.swift in Sources */ = {isa = PBXBuildFile; fileRef = D337713E2B48A829003BD47B /* Root+JSON.swift */; };
 		D33771422B48DB3B003BD47B /* AnyCodable in Frameworks */ = {isa = PBXBuildFile; productRef = D33771412B48DB3B003BD47B /* AnyCodable */; };
 		D33A35A72B2FB1D600FEDC43 /* MediaClock3.swift in Sources */ = {isa = PBXBuildFile; fileRef = D33A35A62B2FB1D600FEDC43 /* MediaClock3.swift */; };
 		D33A35A92B2FB7BE00FEDC43 /* MediaClockManager.swift in Sources */ = {isa = PBXBuildFile; fileRef = D33A35A82B2FB7BE00FEDC43 /* MediaClockManager.swift */; };
@@ -57,6 +56,7 @@
 		D33B52D42A706FD7001A1BA7 /* FlyingSocks in Frameworks */ = {isa = PBXBuildFile; productRef = D33B52D32A706FD7001A1BA7 /* FlyingSocks */; };
 		D33BE9A22B5A3F43000CA189 /* NetworkApplicationDataTypes.swift in Sources */ = {isa = PBXBuildFile; fileRef = D33BE9A12B5A3F43000CA189 /* NetworkApplicationDataTypes.swift */; };
 		D33D595D2B2FD04900963157 /* PortsRepresentable.swift in Sources */ = {isa = PBXBuildFile; fileRef = D33D595C2B2FD04900963157 /* PortsRepresentable.swift */; };
+		D34B5FA42B670576000C8DD1 /* Root+JSON.swift in Sources */ = {isa = PBXBuildFile; fileRef = D34B5FA32B670576000C8DD1 /* Root+JSON.swift */; };
 		D351F2012A7132F8008C5513 /* BoundedDeviceProperty.swift in Sources */ = {isa = PBXBuildFile; fileRef = D351F2002A7132F8008C5513 /* BoundedDeviceProperty.swift */; };
 		D351F2032A727411008C5513 /* Mute.swift in Sources */ = {isa = PBXBuildFile; fileRef = D351F2022A727411008C5513 /* Mute.swift */; };
 		D351F2052A72780A008C5513 /* PanBalance.swift in Sources */ = {isa = PBXBuildFile; fileRef = D351F2042A72780A008C5513 /* PanBalance.swift */; };
@@ -311,7 +311,6 @@
 		D32229CB2ABC270900B56B6B /* Ocp1IORingDeviceEndpoint.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = Ocp1IORingDeviceEndpoint.swift; sourceTree = "<group>"; };
 		D32229CC2ABC270900B56B6B /* Ocp1IORingController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = Ocp1IORingController.swift; sourceTree = "<group>"; };
 		D32229D52ABD26E100B56B6B /* DeviceEndpointRegistrar.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DeviceEndpointRegistrar.swift; sourceTree = "<group>"; };
-		D337713E2B48A829003BD47B /* Root+JSON.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Root+JSON.swift"; sourceTree = "<group>"; };
 		D33A35A62B2FB1D600FEDC43 /* MediaClock3.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MediaClock3.swift; sourceTree = "<group>"; };
 		D33A35A82B2FB7BE00FEDC43 /* MediaClockManager.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MediaClockManager.swift; sourceTree = "<group>"; };
 		D33A35AA2B2FBD0700FEDC43 /* DeviceTimeManager.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DeviceTimeManager.swift; sourceTree = "<group>"; };
@@ -337,6 +336,7 @@
 		D33B52C52A706235001A1BA7 /* DeviceApp.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = DeviceApp.swift; sourceTree = "<group>"; };
 		D33BE9A12B5A3F43000CA189 /* NetworkApplicationDataTypes.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NetworkApplicationDataTypes.swift; sourceTree = "<group>"; };
 		D33D595C2B2FD04900963157 /* PortsRepresentable.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PortsRepresentable.swift; sourceTree = "<group>"; };
+		D34B5FA32B670576000C8DD1 /* Root+JSON.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Root+JSON.swift"; sourceTree = "<group>"; };
 		D351F2002A7132F8008C5513 /* BoundedDeviceProperty.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BoundedDeviceProperty.swift; sourceTree = "<group>"; };
 		D351F2022A727411008C5513 /* Mute.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Mute.swift; sourceTree = "<group>"; };
 		D351F2042A72780A008C5513 /* PanBalance.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PanBalance.swift; sourceTree = "<group>"; };
@@ -697,8 +697,8 @@
 				D33A35B22B2FC4DC00FEDC43 /* ApplicationNetworks */,
 				D351F2062A72999D008C5513 /* Agents */,
 				D33B528F2A6FD332001A1BA7 /* Root.swift */,
+				D34B5FA32B670576000C8DD1 /* Root+JSON.swift */,
 				D33B52AF2A703AEA001A1BA7 /* Root+Commands.swift */,
-				D337713E2B48A829003BD47B /* Root+JSON.swift */,
 				D33D595C2B2FD04900963157 /* PortsRepresentable.swift */,
 				D33B52A62A702D1C001A1BA7 /* Managers */,
 				D33B52A02A702CD6001A1BA7 /* Workers */,
@@ -1575,6 +1575,7 @@
 				D32229D32ABC270900B56B6B /* Ocp1IORingDeviceEndpoint.swift in Sources */,
 				D351F2082A7299B0008C5513 /* Agent.swift in Sources */,
 				D35592602B520DFC000F633B /* ControllerDefaultSubscribing.swift in Sources */,
+				D34B5FA42B670576000C8DD1 /* Root+JSON.swift in Sources */,
 				D33B52A52A702D04001A1BA7 /* Worker.swift in Sources */,
 				D3FAC8EE2B5CFE3A009E2775 /* FirmwareManager.swift in Sources */,
 				D32229D12ABC270900B56B6B /* Ocp1FlyingSocksLogging.swift in Sources */,
@@ -1582,7 +1583,6 @@
 				D32229D02ABC270900B56B6B /* Ocp1FlyingSocksDeviceEndpoint.swift in Sources */,
 				D35592622B520E21000F633B /* Logger+Ocp1ControllerInternal.swift in Sources */,
 				D3AFD35C2B30EC0600A72E9A /* CodingManager.swift in Sources */,
-				D337713F2B48A829003BD47B /* Root+JSON.swift in Sources */,
 				D351F2122A729CB0008C5513 /* TimeSource.swift in Sources */,
 				D33B52AC2A702D4D001A1BA7 /* DeviceManager.swift in Sources */,
 				D355925D2B51E5FF000F633B /* DatagramProxyDeviceEndpoint.swift in Sources */,

Tests/SwiftOCADeviceTests/SwiftOCADeviceTests.swift
@@ -285,8 +285,15 @@ final class SwiftOCADeviceTests: XCTestCase {
             }
         }
 
-        let deviceMembers = await device.rootBlock.actionObjects
-        let testBlockMembers = await testBlock.actionObjects
+        let jsonSerializationExpectation =
+            XCTestExpectation(description: "Ensure JSON serialization round-trips")
+        let jsonObject = await device.rootBlock.jsonObject
+        let jsonResultData = try JSONSerialization.data(withJSONObject: jsonObject)
+        let decoded = try JSONSerialization.jsonObject(with: jsonResultData) as! [String: Any]
+        XCTAssertEqual(decoded as NSDictionary, jsonObject as NSDictionary)
+        jsonSerializationExpectation.fulfill()
+        await fulfillment(of: [jsonSerializationExpectation], timeout: 1)
+
         let connection = await OcaLocalConnection(listener)
         Task { await listener.run() }
         try await connection.connect()
@@ -304,6 +311,7 @@ final class SwiftOCADeviceTests: XCTestCase {
         let controllerExpectation =
             XCTestExpectation(description: "Check rootBlock controller properties")
         let members = try await connection.rootBlock.resolveActionObjects()
+        let deviceMembers = await device.rootBlock.actionObjects
         XCTAssertEqual(members.map(\.objectNumber), deviceMembers.map(\.objectNumber))
         controllerExpectation.fulfill()
 
@@ -318,6 +326,7 @@ final class SwiftOCADeviceTests: XCTestCase {
             ))
         XCTAssertNotNil(clientTestBlock)
         let resolvedClientTestBlockActionObjects = try await clientTestBlock!.resolveActionObjects()
+        let testBlockMembers = await testBlock.actionObjects
         XCTAssertEqual(
             resolvedClientTestBlockActionObjects.map(\.objectNumber),
             testBlockMembers.map(\.objectNumber)