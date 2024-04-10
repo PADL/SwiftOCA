e5c44ed8bec0f737e46b7c5b7c3113c927d478f9
Luke Howard <lukeh@padl.com>
2024-04-10 18:10:27 +1000

    add OcaCounterNotifier client stub

---
+++ Sources/SwiftOCA/OCC/ControlClasses/Agents/OcaCounterNotifier.swift
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2023 PADL Software Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+open class OcaCounterNotifier: OcaAgent {
+    override public class var classID: OcaClassID { OcaClassID("1.2.18") }
+
+    @OcaProperty(
+        propertyID: OcaPropertyID("3.1"),
+        getMethodID: OcaMethodID("3.2"),
+        setMethodID: OcaMethodID("3.3")
+    )
+    public var filterParameters: OcaProperty<OcaCounterNotifierFilterParameters>.PropertyValue
+
+    public func getLastUpdate() async throws -> OcaList<OcaCounterUpdate> {
+        try await sendCommandRrq(methodID: OcaMethodID("3.1"))
+    }
+}
