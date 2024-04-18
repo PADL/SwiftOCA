cf476d5d24f76f4fa91bbba00df90e8b2980f3ed
Luke Howard <lukeh@padl.com>
2024-04-18 15:33:03 +1000

    make Ocp1ConnectionState RawRepresentable

--- Sources/SwiftOCA/OCP.1/Ocp1Connection.swift
+++ Sources/SwiftOCA/OCP.1/Ocp1Connection.swift
@@ -85,7 +85,7 @@ public struct Ocp1ConnectionOptions: Sendable {
     }
 }
 
-public enum Ocp1ConnectionState: Codable, Sendable {
+public enum Ocp1ConnectionState: OcaUint8, Codable, Sendable {
     case notConnected
     case connecting
     case connected
