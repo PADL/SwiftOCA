9c0b0a585b933334649b108de1501bbf8140a364
Luke Howard <lukeh@padl.com>
2025-09-14 09:42:20 +1000

    upgrade IORingSwift to 0.2.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let PlatformProducts: [Product]
 let PlatformTargets: [Target]
 
 #if os(Linux)
-PlatformPackageDependencies = [.package(url: "https://github.com/PADL/IORingSwift", from: "0.1.9")]
+PlatformPackageDependencies = [.package(url: "https://github.com/PADL/IORingSwift", from: "0.2.0")]
 
 PlatformTargetDependencies = [
   .target(
