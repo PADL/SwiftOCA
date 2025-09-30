d43b422f0442240601cf326f39c65fa92121efbe
Luke Howard <lukeh@padl.com>
2025-10-01 02:15:13 +1000

    squash Errno.canceled on datagram receive to Ocp1Error.unknownPduType
    we wish to ignore Errno.canceled as it is a spurious error from io_uring (it
    appears).  we know Ocp1Connection.Monitor will ignore unknownPduType errors in
    receiveMessages(), so we squash the error here
    
    Fixes: PADL/inferno_ui#87

--- Sources/SwiftOCA/OCP.1/Backend/Ocp1IORingConnection.swift
+++ Sources/SwiftOCA/OCP.1/Backend/Ocp1IORingConnection.swift
@@ -142,8 +142,16 @@ public final class Ocp1IORingDatagramConnection: Ocp1IORingConnection {
   }
 
   override public func read(_ length: Int) async throws -> Data {
-    try await withMappedError { socket in
-      try await Data(socket.receive(count: Ocp1MaximumDatagramPduSize))
+    do {
+      return try await withMappedError { socket in
+        try await Data(socket.receive(count: Ocp1MaximumDatagramPduSize))
+      }
+    } catch let error where error as? Errno == Errno.canceled {
+      // something of an abstraction violation: we wish to ignore
+      // Errno.canceled as it is a spurious error from io_uring (it appears).
+      // we know Ocp1Connection.Monitor will ignore unknownPduType errors in
+      // receiveMessages(), so we squash the error here
+      throw Ocp1Error.unknownPduType
     }
   }
 
