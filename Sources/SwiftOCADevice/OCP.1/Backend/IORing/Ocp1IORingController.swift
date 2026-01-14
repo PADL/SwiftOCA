eca67b67f718cf59c03eb63efcb0eb901d3a3181
Luke Howard <lukeh@padl.com>
2026-01-14 13:29:18 +1100

    include peer identifier in stream controller description

--- Sources/SwiftOCADevice/OCP.1/Backend/IORing/Ocp1IORingController.swift
+++ Sources/SwiftOCADevice/OCP.1/Backend/IORing/Ocp1IORingController.swift
@@ -86,7 +86,7 @@ actor Ocp1IORingStreamController: Ocp1IORingControllerPrivate, CustomStringConve
   let notificationSocket: Socket
 
   nonisolated var description: String {
-    "\(type(of: self))(socket: \(socket))"
+    "\(type(of: self))(socket: \(socket), identifier: \(identifier))"
   }
 
   init(
